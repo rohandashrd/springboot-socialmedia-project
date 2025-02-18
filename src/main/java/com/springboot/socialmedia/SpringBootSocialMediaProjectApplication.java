a04804358979c4a76b867ec987a2fe084100033f
rohandashrd <ddash098@gmail.com>
2025-02-18 13:40:10 +0530

    Initial commit

---
+++ src/main/java/com/springboot/socialmedia/SpringBootSocialMediaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocialMediaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSocialMediaProjectApplication.class, args);
+    }
+
+}
